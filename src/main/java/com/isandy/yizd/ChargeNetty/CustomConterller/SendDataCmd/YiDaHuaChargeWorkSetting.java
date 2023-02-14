b4f9a95eae0d10b5545693ef8660c3a01e3d7fe8
isandy <zhangch_@hotmail.com>
2023-02-14 23:23:12 +0800

    新增充电桩工作状态设置

--- src/main/java/com/isandy/yizd/ChargeNetty/CustomConterller/SendDataCmd/YiDaHuaChargeWorkSetting.java
+++ src/main/java/com/isandy/yizd/ChargeNetty/CustomConterller/SendDataCmd/YiDaHuaChargeWorkSetting.java
@@ -1,6 +1,7 @@
 package com.isandy.yizd.ChargeNetty.CustomConterller.SendDataCmd;
 
 import com.isandy.yizd.ChargeNetty.CustomConterller.ChargeContext.YiChargeContext;
+import com.isandy.yizd.ChargeNetty.CustomConterller.Tools.ByteUtils;
 import com.isandy.yizd.ChargeNetty.CustomConterller.Tools.ChannelSendData;
 import com.isandy.yizd.ChargeNetty.CustomConterller.Tools.DaHuaCmdEnum;
 import com.isandy.yizd.ChargeNetty.CustomConterller.Tools.ResData;
@@ -38,6 +39,26 @@ public class YiDaHuaChargeWorkSetting {
                 bcd[6],
                 //是否允许工作 0x00 表示允许正常工作 0x01 表示停止使用，锁定充电桩
                 iw,
+                //充电桩最大允许输出功率 1BIN 表示 1%，最大 100%，最小
+                //30%
+                max,
+        }, context.getInt_sequence());
+        ChannelSendData.Send(chargemax, channel);
+    }
+
+    public void Start(YiChargeContext context, Channel channel, int ChargePercent) {
+        byte max = (byte) ChargePercent;
+        byte[] bcd = context.getBCD();
+        byte[] chargemax = ResData.responseData(context, DaHuaCmdEnum.充电桩工作参数设置, new byte[]{
+                bcd[0],
+                bcd[1],
+                bcd[2],
+                bcd[3],
+                bcd[4],
+                bcd[5],
+                bcd[6],
+                //是否允许工作 0x00 表示允许正常工作 0x01 表示停止使用，锁定充电桩
+                ByteUtils.toByte(0, 1)[0],
                 //充电桩最大允许输出功率 1BIN 表示 1%，最大 100%，最小
                 //30%
                 max,
